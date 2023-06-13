9394adffe3ad0da5686dd1b9a1f57b5ac768b5c0
Rokoucha <git@rokoucha.net>
2023-06-14 00:25:19 +0900

    PoC

---
+++ go.mod
@@ -0,0 +1,23 @@
+module github.com/rokoucha/switchbot-meter-exporter
+
+go 1.20
+
+require (
+	github.com/jessevdk/go-flags v1.5.0
+	github.com/nasa9084/go-switchbot v1.0.0
+	github.com/prometheus/client_golang v1.15.1
+	golang.org/x/exp v0.0.0-20230522175609-2e198f4a06a1
+)
+
+require (
+	github.com/beorn7/perks v1.0.1 // indirect
+	github.com/cespare/xxhash/v2 v2.2.0 // indirect
+	github.com/golang/protobuf v1.5.3 // indirect
+	github.com/google/uuid v1.3.0 // indirect
+	github.com/matttproud/golang_protobuf_extensions v1.0.4 // indirect
+	github.com/prometheus/client_model v0.3.0 // indirect
+	github.com/prometheus/common v0.42.0 // indirect
+	github.com/prometheus/procfs v0.9.0 // indirect
+	golang.org/x/sys v0.6.0 // indirect
+	google.golang.org/protobuf v1.30.0 // indirect
+)
---
+++ main.go
@@ -0,0 +1,150 @@
+package main
+
+import (
+	"context"
+	"fmt"
+	"net/http"
+	"os"
+	"time"
+
+	"github.com/jessevdk/go-flags"
+	"github.com/nasa9084/go-switchbot"
+	"github.com/prometheus/client_golang/prometheus"
+	"github.com/prometheus/client_golang/prometheus/promhttp"
+	"golang.org/x/exp/slices"
+	"golang.org/x/exp/slog"
+)
+
+type options struct {
+	Port *uint `short:"p" long:"port" description:"Port number to listen, default: 8080"`
+}
+
+const (
+	namespace = "switchbot"
+	subsystem = "meter"
+)
+
+var labels = []string{"deviceName", "deviceType", "hubDeviceId"}
+
+func handleProbe(client *switchbot.Client, logger *slog.Logger) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		ctx, cancel := context.WithTimeout(r.Context(), time.Second*5)
+		defer cancel()
+
+		params := r.URL.Query()
+		target := params.Get("target")
+		if target == "" {
+			http.Error(w, "target is required", http.StatusBadRequest)
+			return
+		}
+
+		devices, _, err := client.Device().List(ctx)
+		if err != nil {
+			slog.Error("failed to get list", "err", err)
+			http.Error(w, "failed to get list", http.StatusInternalServerError)
+			return
+		}
+
+		i := slices.IndexFunc(devices, func(d switchbot.Device) bool {
+			return d.ID == target
+		})
+		if i < 0 {
+			http.Error(w, "target not found", http.StatusNotFound)
+			return
+		}
+		device := devices[i]
+
+		status, err := client.Device().Status(ctx, device.ID)
+		if err != nil {
+			slog.Error("failed to get device status", "target", target, "err", err)
+			http.Error(w, "failed to get device status", http.StatusInternalServerError)
+			return
+		}
+
+		battery := prometheus.NewGaugeVec(prometheus.GaugeOpts{
+			Namespace: namespace,
+			Subsystem: subsystem,
+			Name:      "battery",
+			Help:      "the current battery level, 0-100",
+		}, labels)
+		battery.WithLabelValues(
+			device.Name,
+			string(device.Type),
+			device.Hub,
+		).Set(float64(status.Battery))
+
+		humidity := prometheus.NewGaugeVec(prometheus.GaugeOpts{
+			Namespace: namespace,
+			Subsystem: subsystem,
+			Name:      "humidity",
+			Help:      "humidity percentage",
+		}, labels)
+		humidity.WithLabelValues(
+			device.Name,
+			string(device.Type),
+			device.Hub,
+		).Set(float64(status.Humidity))
+
+		tempareture := prometheus.NewGaugeVec(prometheus.GaugeOpts{
+			Namespace: namespace,
+			Subsystem: subsystem,
+			Name:      "tempareture",
+			Help:      "temperature in celsius",
+		}, labels)
+		tempareture.WithLabelValues(
+			device.Name,
+			string(device.Type),
+			device.Hub,
+		).Set(float64(status.Temperature))
+
+		registry := prometheus.NewRegistry()
+		registry.MustRegister(battery, humidity, tempareture)
+
+		h := promhttp.HandlerFor(registry, promhttp.HandlerOpts{})
+		h.ServeHTTP(w, r)
+	}
+}
+
+func main() {
+	logger := slog.New(slog.NewTextHandler(os.Stdout, nil))
+
+	var opts options
+	if _, err := flags.Parse(&opts); err != nil {
+		flagsErr := err.(*flags.Error)
+		if flagsErr.Type == flags.ErrHelp {
+			os.Exit(0)
+		}
+		os.Exit(2)
+	}
+
+	var port string
+	if opts.Port != nil {
+		port = fmt.Sprintf(":%d", *opts.Port)
+	} else {
+		port = ":8080"
+	}
+
+	token := os.Getenv("SWITCHBOT_OPENTOKEN")
+	if token == "" {
+		logger.Error("Please set SWITCHBOT_OPENTOKEN env variable")
+		os.Exit(1)
+	}
+
+	secret := os.Getenv("SWITCHBOT_SECRETKEY")
+	if secret == "" {
+		logger.Error("Please set SWITCHBOT_SECRETKEY env variable")
+		os.Exit(1)
+	}
+
+	client := switchbot.New(token, secret)
+
+	http.Handle("/metrics", promhttp.Handler())
+	http.HandleFunc("/probe", handleProbe(client, logger))
+
+	logger.Info("Starting server on port " + port)
+
+	err := http.ListenAndServe(port, nil)
+	if err != nil {
+		logger.Error("error", err)
+	}
+}
